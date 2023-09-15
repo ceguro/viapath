11cced871f6cc71db399ff3021db4b74b5a16cc3
Carlos Gutiérrez <ceguro10@gmail.com>
2023-09-15 12:54:52 -0300

    feat: add global exception handler

---
+++ src/main/java/com/challenge/viapath/error/handle/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.challenge.viapath.error.handle;
+
+import com.challenge.viapath.error.exception.BadRequestException;
+import com.challenge.viapath.error.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Object> handleBadRequestException(BadRequestException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleOtherExceptions(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An internal server error has occurred, please contact support");
+    }
+}
+
+
+
+
+
+
+
